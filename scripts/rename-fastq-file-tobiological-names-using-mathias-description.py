37466d6bd9c4d863ca65f05749ef62215b4258e6
threadmapper <threadmapper@gmail.com>
2023-05-18 15:48:26 +0100

    Add files via upload

--- scripts/rename-fastq-file-tobiological-names-using-mathias-description.py
+++ scripts/rename-fastq-file-tobiological-names-using-mathias-description.py
@@ -6,10 +6,12 @@
 Renaming files using the Descriptive names
 
 """
+from collections import defaultdict
 
 head = ['experiment', 'MD5', 'FASTQ', 'biological-name']
 records = 0
-with open('checksums_MLN.csv') as inp, open('checksums_MLN-renamed.csv', 'w') as outf:
+seen = defaultdict(list)
+with open('checksums_MLN-suggested-fixed.csv') as inp, open('checksums_MLN-suggested-fixed-renamed.csv', 'w') as outf:
     next(inp)
     outf.write(','.join(head) + '\n')
     for line in inp:
@@ -18,9 +20,16 @@ with open('checksums_MLN.csv') as inp, open('checksums_MLN-renamed.csv', 'w') as
         experi = d['experiment'].replace('(', '').replace(')', '')
         new_fq_name = '-'.join(experi.split()) + '.fq.gz'
         d['biological-name'] = new_fq_name
+        seen[new_fq_name].append(d)
         outf.write(','.join([d[h] for h in head]) + '\n')
+        print(' '.join(['cp', '../' + d['FASTQ'].strip(), new_fq_name]))
         records += 1
-        print(' '.join(['cp', '../' + d['FASTQ'], new_fq_name]))
 
+# check for duplicates
+for new_fq_name, rows in seen.items():
+    if len(rows) > 1:
+        print(rows)
+        for a in rows:
+            print(' '.join(a))
 print('# records written: ', records)
 print('# Done')
